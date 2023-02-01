4adcb9a48f52e39c2a7f32cf60a82c5033a1b83e
kcalixto <kauacalixtocontato@gmail.com>
2023-01-31 22:54:32 -0300

    added all database types

--- services/interfaces/database.ts
+++ services/interfaces/database.ts
@@ -23,7 +23,7 @@ export interface DBAddress extends RowDataPacket {
     additional_info: string;
     created_at: string;
     updated_at: string;
-    active: string;
+    active: boolean;
 }
 
 export interface DBCardInfo extends RowDataPacket {
@@ -36,7 +36,21 @@ export interface DBCardInfo extends RowDataPacket {
     card_flag: string;
     created_at: string;
     updated_at: string;
-    active: string;
+    active: boolean;
+}
+
+export interface DBBankInfo extends RowDataPacket {
+    bank_info_id: number;
+    bank_info_uuid: string;
+    document: string;
+    bank_number: string;
+    agency: string;
+    agency_digit: string;
+    account_number: string;
+    account_number_digit: string;
+    created_at: string;
+    updated_at: string;
+    active: boolean;
 }
 
 export interface DBPayment extends RowDataPacket {
@@ -51,7 +65,7 @@ export interface DBPayment extends RowDataPacket {
     user_uuid: string;
     created_at: string;
     updated_at: string;
-    active: string;
+    active: boolean;
 }
 
 export interface DBPet extends RowDataPacket {
@@ -60,12 +74,122 @@ export interface DBPet extends RowDataPacket {
     user_id: number;
     full_name: string;
     birth_date: string;
-    size: number; // double
-    weight: number; // double
+    size: number;
+    weight: number;
     description: string;
     specie_id: number;
     race_id: number;
     created_at: string;
     updated_at: string;
-    active: string;
+    active: boolean;
+}
+export interface DBSpecies extends RowDataPacket {
+    specie_id: number;
+    specie: string;
+    created_at: string;
+    updated_at: string;
+    active: boolean;
+}
+
+export interface DBRaces extends RowDataPacket {
+    race_id: number;
+    race: string;
+    specie_id: number;
+    created_at: string;
+    updated_at: string;
+    active: boolean;
+}
+
+export interface DBCompany extends RowDataPacket {
+    company_id: number;
+    company_uuid: string;
+    document: string;
+    corporate_name: string;
+    social_name: string;
+    contact_email: string;
+    contact_number: string;
+    created_at: string;
+    updated_at: string;
+    active: boolean;
+}
+
+export interface DBCompanyBranch extends RowDataPacket {
+    company_branch_id: number;
+    company_branch_uuid: string;
+    company_uuid: number;
+    contact_email: string;
+    contact_number: string;
+    bank_info_uuid: string;
+    created_at: string;
+    updated_at: string;
+    active: boolean;
+}
+
+export interface DBDoctor extends RowDataPacket {
+    doctor_id: number;
+    doctor_uuid: string;
+    full_name: string;
+    birth_date: string;
+    contact_email: string;
+    contact_number: string;
+    expertise: string;
+    description: string;
+    company_branch_id: number;
+    created_at: string;
+    updated_at: string;
+    active: boolean;
+}
+
+export interface DBServices extends RowDataPacket {
+    service_id: number;
+    service_uuid: string;
+    name: string;
+    created_at: string;
+    updated_at: string;
+    active: boolean;
+}
+
+export interface DBDoctorServices extends RowDataPacket {
+    doctor_service_id: number;
+    doctor_id: number;
+    service_id: number;
+    available: boolean;
+    created_at: string;
+    updated_at: string;
+    active: boolean;
+}
+
+export interface DBAvailability extends RowDataPacket {
+    availabillity_id: number;
+    doctor_service_id: number;
+    start_time: string;
+    end_time: string;
+    appointment_id: number;
+    created_at: string;
+    updated_at: string;
+    active: boolean;
+}
+
+export interface DBAppointment extends RowDataPacket {
+    appointment_id: number;
+    appointment_uuid: string;
+    pet_uuid: string;
+    doctor_uuid: string;
+    notification_uuid: string;
+    timestamp: string;
+    created_at: string;
+    updated_at: string;
+    active: boolean;
+}
+
+export interface DBNotification extends RowDataPacket {
+    notification_id: number;
+    notification_uuid: string;
+    company_uuid: string;
+    info: string;
+    expires_at: string;
+    delivered: boolean;
+    created_at: string;
+    updated_at: string;
+    active: boolean;
 }
--- services/interfaces/types.ts
+++ services/interfaces/types.ts
@@ -27,3 +27,119 @@ export type CardInfo = {
     cardFlag: string;
 }
 
+export type BankInfo = {
+    id: number;
+    uuid: string;
+    document: string;
+    bankNumber: string;
+    agency: string;
+    agencyDigit: string;
+    accountNumber: string;
+    accountNumberDigit: string;
+}
+
+export type Payment = {
+    id: number;
+    uuid: string;
+    paymentMethod: string;
+    installments: number;
+    installmentDueDate: number;
+    billTotal: number;
+    doctorUUID: string;
+    petUUID: string;
+    userUUID: string;
+}
+
+export type Pet = {
+    id: number;
+    uuid: string;
+    userID: number;
+    fullName: string;
+    birthDate: string;
+    size: number;
+    weight: number;
+    description: string;
+    specieID: number;
+    raceID: number;
+}
+
+export type Species = {
+    id: number;
+    specie: string;
+}
+
+export type Races = {
+    id: number;
+    race: string;
+    specieID: number;
+}
+
+export type Company = {
+    id: number;
+    uuid: string;
+    document: string;
+    corporateName: string;
+    socialName: string;
+    contactEmail: string;
+    contactNumber: string;
+}
+
+export type CompanyBranch = {
+    id: number;
+    uuid: string;
+    companyUUID: number;
+    contactEmail: string;
+    contactNumber: string;
+    bankInfoUUID: string;
+}
+
+export type Doctor = {
+    id: number;
+    uuid: string;
+    fullName: string;
+    birthDate: string;
+    contactEmail: string;
+    contactNumber: string;
+    expertise: string;
+    description: string;
+    companyBranchID: number;
+}
+
+export type Services = {
+    id: number;
+    uuid: string;
+    name: string;
+}
+
+export type DoctorServices = {
+    id: number;
+    doctorID: number;
+    serviceID: number;
+    available: boolean;
+}
+
+export type Availability = {
+    id: number;
+    doctorServiceID: number;
+    startTime: string;
+    endTime: string;
+    appointmentID: number;
+}
+
+export type Appointment = {
+    id: number;
+    uuid: string;
+    petUUID: string;
+    doctorUUID: string;
+    notificationUUID: string;
+    timestamp: string;
+}
+
+export type Notification = {
+    id: number;
+    uuid: string;
+    companyUUID: string;
+    info: string;
+    expiresAt: string;
+    delivered: boolean;
+}
